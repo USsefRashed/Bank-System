6efb2dc0aa9e442abf0cba1f77c471bd4fd2a59d
USsefRashed <USsefRashed@gmail.com>
2022-04-21 19:59:45 -0700

    init_reFactor_1

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 #include<array>
 #include"Bank.h"
 using namespace std;
-int main()
+
+void operations(Bank Client)
 {
-    char ans,cont;int size;
+    char ans;
     cout<<"@------Welcome to our Bank system ------@\n\n";
     cout<<"-----------\nEnter : (A)dd Account\n-----------\nEnter : (I)Display Client by Id\n-----------\nEnter : (D)isplayAccounts\n-----------\n"<<endl;
     cin>>ans;
-cout<<"How Many Account You want to add : ";cin>>size;
-    Bank Client=Bank(size);
-    while (true)
-    {
     if(ans=='d'||ans=='D')
     {
         Client.getAccounts();
@@ -23,13 +20,24 @@ cout<<"How Many Account You want to add : ";cin>>size;
     else if(ans=='I'||ans=='i')
     {
         int id;
-        cout<<"Enter the Client Id : ";cin>>id;Client.getById(id);
+        cout<<"Enter the Client Id : ";cin>>id;
+        Client.getById(id);
     }
     else
     {
         cout<<"Invalid value ! ";
     }
-        cout<<"Press any key to continue";cin>>cont;
+}
+int main()
+{
+    char cont;int size;
+    cout<<"How Many Account You want to add : ";cin>>size;
+    Bank Client=Bank(size);
+    while (true)
+    {
+        operations(Client);
+
+        cout<<"Press any key to continue & (E/e) to exit : ";cin>>cont;
         if(cont=='e'||cont=='E')
         break;
     }
