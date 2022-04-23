10b15e40e46158d678140ab345caff5be9eb1592
USsefRashed <USsefRashed@gmail.com>
2022-04-23 07:15:33 -0700

    Add file-printer

---
+++ Bank.h
@@ -0,0 +1,292 @@
+#include<iostream>
+#include<array>
+#include<string>
+#include<ctime>
+#include <fstream>
+using namespace std;
+class Bank
+{
+protected:
+
+    int Id,*Ids,Size;
+    double *Balance;
+    string *FirstName;
+    string *LastName;
+    string *Date;
+public:
+
+Bank(int size);
+bool isExist(int id);
+string getDateTime();
+void setAccounts();
+void getAccounts();
+void getById(int id);
+void ModifyById(int id);
+void DeleteById(int id);
+void ModifyFName(int id);
+void ModifyLName(int id);
+void ModifBalance(int id);
+void Writetxtfile();
+
+};
+
+Bank::Bank(int size)
+{
+    try
+    {
+        if(size<=0)
+        {
+            throw"Sorry the Accounts number should be larger than 0";
+        }
+    }
+    catch(const char* msg)
+    {
+        while (true)
+        {
+        cerr << msg <<endl;
+        cout<<"Renter the number of accounts : "<<endl;
+        cin>>size;
+        if(!(size<=0))
+        {break;}       
+        }
+    }
+    Size=size;
+    Id=-1;
+    Ids=new int[Size];
+    FirstName=new string[Size];
+    LastName=new string[Size];
+    Balance=new double[Size];
+    Date=new string[Size];
+}
+
+bool Bank::isExist(int id)
+{
+    for (int i = 0; i < Size; i++)
+    {
+        if(Ids[i]==id &&Ids[id]!=-1 && FirstName[id].empty()==false)
+        return 1;
+    }
+    return 0;
+}
+
+string Bank::getDateTime()
+{
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    tm *gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    return dt;
+}
+
+void Bank::setAccounts(){
+    for (int i = 0; i < Size; i++)
+    {
+        Ids[i]=++Id;
+        Date[i]=getDateTime();
+        cout<<"Date of process : "<<Date[i];
+        cout<<"Enter the Client First name : ";cin>>FirstName[i];
+        cout<<"Enter the Client Last name : ";cin>>LastName[i];
+        cout<<"Enter the Client Balance : ";cin>>Balance[i];
+    }
+    Id++;
+    
+}
+void Bank::getAccounts(){
+    if(Id==-1)
+    {
+        cout<<"There is not accounts added yet !"<<endl;
+    }
+    else
+    {
+    cout<<"\n*--------------------ACCOUNTS--------------------*\n";
+        for (int i = 0; i < Size; i++)
+        {
+            if (FirstName[i].empty()==true)
+            {
+                continue;
+            }
+            else
+            {
+            cout<<"\n==============================\n\n";
+            cout<<"Client Id : "<<Ids[i];
+            cout<<"Client : "<<FirstName[i]<<" "<<LastName[i]<<endl;
+            cout<<"Balance : "<<Balance[i]<<endl;
+            cout<<"Process Date"<<Date[i];
+            cout<<"\n==============================\n\n";
+            }
+            
+        }
+    }
+    
+}
+void Bank::getById(int id)
+{
+    if(id<0 || id>Size)
+    {
+        cout<<"out of range !\n\n";
+        while (true)
+        {
+            cout<<"Renter the client id : ";cin>>id;
+            if(!(id<0 || id<Size))
+            break;
+        }
+    
+    }
+    if (FirstName[id].empty()==true)
+    {
+        cout<<"There is no data for this id !"<<endl;
+    }
+    else
+    {
+        cout<<"ID : "<<Ids[id]<<endl;
+        cout<<"Client : "<<FirstName[id]<<endl;
+        cout<<"Balance :"<<Balance[id]<<endl;
+        cout<<"Date of process"<<Date[id]<<endl;
+    }
+    
+}
+void Bank::ModifyById(int id)
+{
+    char cho;
+    cout<<"\n============================================"<<endl;
+    cout<<"Which U want to modify ? \n (F)irstName\n(L)astName\n(B)alance\n";
+    cout<<"============================================"<<endl;
+    cin>>cho;
+    if (cho=='B'||cho=='b')
+    {
+        ModifBalance(id);
+        cout<<"Client is modified sucessfully !"<<endl;
+
+    }
+    else if(cho=='F'||cho=='f')
+    {
+        ModifyFName(id);
+        cout<<"Client is modified sucessfully !"<<endl;
+    }
+    else if(cho=='L'||cho=='l')
+    {
+        ModifyLName(id);
+        cout<<"Client is modified sucessfully !"<<endl;
+    
+    }
+    else{
+        cout<<"Invalid choice !"<<endl;
+    }
+    
+}
+void Bank::DeleteById(int id)
+{
+    if (isExist(id)==false)
+    {
+        cout<<"the client is not exist !"<<endl;
+    }
+    else
+    {
+        Ids[id]=-1;
+        FirstName[id].clear();
+        LastName[id].clear();
+        Balance[id]=-1;
+        cout<<"Client is deleted sucessfully !"<<endl;
+    }
+    
+}
+
+void Bank::ModifyFName(int id)
+{
+    try
+    {
+        if(isExist(id)==false)
+        {
+        throw "Sorry  this Client is not exist !";
+        }
+        else
+        {
+            string name;
+            cout<<"Enter the new First name of user : ";cin>>name;
+            FirstName[id]=name;
+        }
+
+    }
+    catch(const char*msg)
+    {
+        std::cerr <<msg<< '\n';
+    }
+    
+
+}
+void Bank::ModifyLName(int id)
+{
+        try
+    {
+        if(isExist(id)==false)
+        {
+        throw "Sorry  this Client is not exist !";
+        }
+        else
+        {
+            string name;
+            cout<<"Enter the new Last name of user : ";cin>>name;
+            LastName[id]=name;
+        }
+
+    }
+    catch(const char*msg)
+    {
+        std::cerr <<msg<< '\n';
+    }
+}
+void Bank::ModifBalance(int id)
+{
+        try
+    {
+        if(isExist(id)==false)
+        {
+        throw "Sorry  this Client is not exist !";
+        }
+        else
+        {
+            int balance;
+            cout<<"Enter the new balance of user : ";cin>>balance;
+            Balance[id]=balance;
+        }
+
+    }
+    catch(const char*msg)
+    {
+        std::cerr <<msg<< '\n';
+    }
+}
+
+
+void Bank::Writetxtfile() {
+
+    fstream filetxt;
+    filetxt.open("accounts" , ios::out);
+    if (filetxt.is_open()){
+        if(Id==-1)
+        {
+            filetxt<<"There is not accounts added yet !"<<endl;
+        }
+        else
+        {
+        filetxt<<"\n*--------------------ACCOUNTS--------------------*\n";
+            for (int i = 0; i < Size; i++)
+            {
+                if (FirstName[i].empty()==true)
+                {
+                    continue;
+                }
+                else
+                {
+                filetxt<<"\n==============================\n\n";
+                filetxt<<"Client Id : "<<Ids[i]<<endl;
+                filetxt<<"Client : "<<FirstName[i]<<" "<<LastName[i]<<endl;
+                filetxt<<"Balance : "<<Balance[i]<<endl;
+                filetxt<<"Process Date"<<Date[i];
+                filetxt<<"\n==============================\n\n";
+                }
+                
+            }
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include <bits\stdc++.h>
+#include<array>
+#include<conio.h>
+#include<string.h>
+#include"Bank.h"
+using namespace std;
+
+void operations(Bank &Client)
+{
+    char ans;
+    cout<<"@------Welcome to our Bank system ------@\n\n";
+    cout<<"-----------\nEnter : (A)dd Account\n-----------\nEnter : (I)Display Client by Id\n-----------\nEnter : (D)isplay Accounts\n-----------\n(M)odify Account\n-----------\n"<<endl;
+    cout<<"(R)emove Client\n-----------\n";
+    cin>>ans;
+    if(ans=='d'||ans=='D')
+    {
+        Client.getAccounts();
+    }
+    else if(ans=='a'||ans=='A')
+    {
+        Client.setAccounts();
+    }
+    else if(ans=='I'||ans=='i')
+    {
+        int id;
+        cout<<"Enter the Client Id : ";cin>>id;
+        Client.getById(id);
+    }
+    else if(ans=='m'||ans=='M')
+    {
+        int id;
+        cout<<"Enter Client ID : ";cin>>id;
+        Client.ModifyById(id);
+    }
+    else if(ans=='r'||ans=='R')
+    {
+        int id;
+        cout<<"Enter id to remove : ";cin>>id;
+        Client.DeleteById(id);
+    }
+    else
+    {
+        cout<<"Invalid value ! ";
+    }
+}
+int main()
+{
+    char cont;int size;
+    cout<<"How Many Account You want to add : ";cin>>size;
+    Bank Client=Bank(size);
+    while (true)
+    {
+        operations(Client);
+
+        cout<<"Press any key to continue or (E/e) to exit : ";
+        cont = getche();
+        system("cls");
+        if(cont=='e'||cont=='E'){
+            
+            cout << "you  want write accounts information in .txt file (y/n) ? ";
+            cont = tolower(getche());
+            if (cont == 'y' ){
+                Client.Writetxtfile();
+            }
+            
+            break;
+        }
+        
+    }
+    
+
+}
